cf0eb778b1703e0cce2fe693690d89d2a7d10142
daniedu <81932800+daniedu@users.noreply.github.com>
2025-08-07 09:14:19 -0500

    updating config to use lazyvim separately

---
+++ lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  -- List all your individual plugin files here
+  require("plugins.colorscheme"),
+  require("plugins.lualine"),
+  require("plugins.treesitter"),
+  -- require("plugins.whichkey"),
+  -- Add other plugins here, each in its own file
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,57 @@
 -- lua/plugins/treesitter.lua
+return {
+  -- The plugin specification
+  "nvim-treesitter/nvim-treesitter",
 
-require('nvim-treesitter.configs').setup({
-  -- A list of languages to install.
-  -- You can add or remove languages you work with here.
-  ensure_installed = {
-    "c",
-    "lua",
-    "vim",
-    "vimdoc",
-    "javascript",
-    "html",
-    "css",
-    "python",
-  },
+  -- The config function for setting up the plugin
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      -- A list of languages to install.
+      -- You can add or remove languages you work with here.
+      ensure_installed = {
+        "c",
+        "c_sharp",
+        "lua",
+        "vim",
+        "vimdoc",
+        "bash",
+        "markdown",
+        "jsdoc",
+        "javascript",
+        "typescript",
+        "json",
+        "yaml",
+        "tsx",
+        "html",
+        "css",
+      },
 
-  -- Install languages synchronously (only applied when `ensure_installed` is changed)
-  sync_install = false,
+      -- Install languages synchronously (only applied when `ensure_installed` is changed)
+      sync_install = false,
 
-  -- Automatically install missing parsers when you open a file of that type
-  auto_install = true,
+      -- Automatically install missing parsers when you open a file of that type
+      auto_install = true,
 
-  -- Enable syntax highlighting
-  highlight = {
-    enable = true,
-  },
+      -- Enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
 
-  -- Enable indentation
-  indent = {
-    enable = true,
-  },
+      -- Enable indentation
+      indent = {
+        enable = true,
+      },
 
-  -- You can also enable incremental selection
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<leader>si',
-      node_incremental = '<leader>sn',
-      scope_incremental = '<leader>ss',
-      node_decremental = '<leader>sd',
-    },
-  },
-})
+      -- You can also enable incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<leader>si',
+          node_incremental = '<leader>sn',
+          scope_incremental = '<leader>ss',
+          node_decremental = '<leader>sd',
+        },
+      },
+    })
+  end,
+}
