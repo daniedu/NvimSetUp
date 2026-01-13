556fa884263b8afbb74751d3ea12dd82a791ecf4
andresDevComp <andresDevComp@gmail.colm>
2026-01-13 09:41:56 -0500

    updating with more usefull plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,14 @@
 return {
   -- List all your individual plugin files here
+  require("plugins.mason"),
+  require("plugins.wich-key"),
+  require("plugins.gitsigns"),
   require("plugins.colorscheme"),
   require("plugins.treesitter"),
   require("plugins.lualine"),
   require("plugins.telescope"),
+  require("plugins.conform"),
+  require("plugins.nvim-colorizer"),
   -- require("plugins.whichkey"),
   -- Add other plugins here, each in its own file
 }
